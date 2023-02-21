6b727a0b6d92bddd8d4c670d0eabf608c405838d
Beck <b.tadesse.77@gmail.com>
2023-02-21 10:28:47 +0300

    [UNTESTED] Git config support

--- apply.sh
+++ apply.sh
@@ -3,15 +3,24 @@
 # Define the config directories
 nvim_config_dir=~/.config/nvim
 zsh_config_dir=~/.oh-my-zsh
+git_config_file=~/.gitconfig
 
-echo "Warning: This will overwrite any existing configs in the following folders"
+echo "Warning: This will overwrite any existing configs in the following files and folders"
 echo "  ~/.config/nvim"
 echo "  ~/.oh-my-zsh/custom"
+echo "  ~/.gitconfig"
 read -p "Are you sure you want to continue? (y/N) " -n 1 -rs
 echo
 echo
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
+	# Delete the .gitconfig file if it exists
+	if [ -f "$git_config_file" ]; then
+		rm "$git_config_file"
+	fi
+	# Copy over the .gitconfig file from repo
+	cp git/.gitconfig "$git_config_file"
+
 	# Delete the nvim config directory if it exists
 	if [ -d "$nvim_config_dir" ]; then
 		rm -r "$nvim_config_dir"
--- pull.sh
+++ pull.sh
@@ -3,6 +3,17 @@
 # Define the config directories
 nvim_config_dir=~/.config/nvim
 zsh_config_dir=~/.oh-my-zsh
+git_config_filename=.gitconfig
+git_config_file=~/$git_config_filename
+
+# Copy over .gitconfig if exists
+if [ -f "$git_config_file" ]; then
+	# Create the git directory if it doesn't already exist
+	mkdir -p git
+	# Copy the config file
+	cp "$git_config_file" git/"$git_config_filename"
+	echo "Git global config copied."
+fi
 
 # Check if the config directories exist
 if [ -d "$nvim_config_dir" ]; then
